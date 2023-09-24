3faa3ffd81efe10cac4d8c8de603cb8c605051de
gurworla <rlawogur0533@naver.com>
2023-09-24 18:03:25 +0900

    404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import { createRouter } from "../core/hyeok";
 import Home from "./Home";
 import Movie from "./Movie";
 import About from "./About";
+import NotFound from "./NotFound";
 
 export default createRouter([
   { path: "#/", component: Home },
   { path: "#/movie", component: Movie },
   { path: "#/about", component: About },
+  { path: ".*", component: NotFound },
 ]);
